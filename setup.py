38c45e945ae4c497f20111d87c5241bdc80dbeef
Adam Kewley <akewley@ast.cam.ac.uk>
2018-04-13 10:20:49 +0100

    Trivial version update to trigger travis

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startsw
 
 setup(
     name='lor',
-    version='0.0.2-SNAPSHOT',
+    version='0.0.2-SNAPSHOT+1',
     description='A framework for rapidly developing pipelines with Luigi',
     long_description=long_description,
     author='Adam Kewley',
