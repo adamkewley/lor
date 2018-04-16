6cb541ea7faca4f696a8acca8371fef04f0568ce
Adam Kewley <contact@adamkewley.com>
2018-04-16 18:49:40 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startsw
 
 setup(
     name='lor',
-    version='0.0.2',
+    version='0.0.3',
     description='A framework for rapidly developing pipelines with Luigi',
     long_description=long_description,
     author='Adam Kewley',
